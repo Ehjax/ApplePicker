4b56b19220a6a3456f3b1ea2ba6678c1c66a8335
Ben Fasinski <Bfasinski@gmail.com>
2016-09-09 17:49:33 -0400

    Added Scores

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Basket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public GUIText		scoreGT;
+
+
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,11 +26,29 @@ public class Basket : MonoBehaviour {
 		this.transform.position = pos;
 	}
 
+	// Use this for initialization
+	void Start () {
+		//find a reference to the ScoreCounter GameObject
+		GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		// Get the GUIText component of that GameObject
+		scoreGT = scoreGO.GetComponent<GUIText> ();
+		//Set the starting number of points to 0
+		scoreGT.text = "0";
+
+	}
+
 	void OnCollisionEnter (Collision coll) {
 		//find out what hit this basket
 		GameObject collidedWith = coll.gameObject;
 		if (collidedWith.tag == "Apple") {
 			Destroy (collidedWith);
 		}
+
+		//parse the text of the scoreGT into an int
+		int score = int.Parse (scoreGT.text);
+		//add points for catching the apple
+		score += 100;
+		// convert the score back to a string and display it
+		scoreGT.text = score.ToString ();
 	}
 }
